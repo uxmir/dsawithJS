33548b42698ac59d076e46d79141977a190e33d7
Mir Moniruzzaman <mirmuniruzzaman303@gmail.com>
2026-01-26 23:56:04 +0600

    array travrerse ad access done

---
+++ array_traverse_acccess.js
@@ -0,0 +1,27 @@
+//traverse and access
+const data=[2,3,4,8,6,12,98]
+for(let i=0;i<data.length;i++){
+  console.log(data[i]) 
+  console.log(data[3]) 
+}
+
+//2st problem:total by adding each value 
+let item= [2,3,4,8,6,12,98];
+let sum=0
+for(let i=0;i<item.length;i++){
+   sum+=item[i]
+   console.log(sum)
+}
+//3rd problem  finding evenvalue
+for (let i=0; i<item.length;i++){
+    if(item[i]%2===0){
+        console.log(item[i])
+    }
+}
+//finding odd value
+for (let i=0;i<item.length;i++){
+    if(item[i]%2 !==0){
+        console.log(item[i])
+    }
+ 
+}
