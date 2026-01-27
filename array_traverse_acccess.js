a9c2adfeab00bcdaaa4bc320ac4f4ab25d1f0145
Mir Moniruzzaman <mirmuniruzzaman303@gmail.com>
2026-01-27 10:33:41 +0600

    i found length in array

--- array_traverse_acccess.js
+++ array_traverse_acccess.js
@@ -1,11 +1,11 @@
-//traverse and access
+// //traverse and access
 const data=[2,3,4,8,6,12,98]
 for(let i=0;i<data.length;i++){
   console.log(data[i]) 
   console.log(data[3]) 
 }
 
-//2st problem:total by adding each value 
+// //2st problem:total by adding each value 
 let item= [2,3,4,8,6,12,98];
 let sum=0
 for(let i=0;i<item.length;i++){
@@ -18,10 +18,15 @@ for (let i=0; i<item.length;i++){
         console.log(item[i])
     }
 }
+//finding array length
+console.log(item.length)
+
 //finding odd value
 for (let i=0;i<item.length;i++){
     if(item[i]%2 !==0){
         console.log(item[i])
     }
- 
 }
+
+
+
