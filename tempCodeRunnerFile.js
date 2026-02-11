b85938656fa8088a49ff37d3f0f6c378c57e6956
Mir Moniruzzaman <mirmuniruzzaman303@gmail.com>
2026-02-11 20:41:35 +0600

    shopping cart manupulation is done

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,20 +1,4 @@
-//ber kora price and discount
-// function getPrice(price,discount){
-//  if(price<=0)  throw new Error('the number is negative');
-
-//  if(price && discount >=50) {
-//     console.log('price is to high')
-//  }
-//  else{
-//     const discountPrice=(discount/100)*price
-//     console.log(`this is percentage result:${discountPrice}`)
-//     const total_result=price-discountPrice
-//     console.log(total_result)
-//  }
+if(result>40){
+//   const discount=(result/10)*100
+//   console.log(discount)
 // }
-// getPrice(400,40)
-
-// //finding s in names
-// const names = ["Abir", "Sabbir", "Sharmin", "Sujon", "Roni"];
-// const filteredName=names.filter((val)=>val.charAt(0)==="S")
-// console.log(filteredName)
