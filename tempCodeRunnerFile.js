c3d547d47c0a92e0ca39d9ace6bed9d2fa4ee073
Mir Moniruzzaman <mirmuniruzzaman303@gmail.com>
2026-02-15 12:19:25 +0600

    The Even-Odd Sum (Array Logic) iss done

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,8 +1,1 @@
-function reverseString(string){
-  const str=string;
-  const a=str.split(' ').reverse().join( ' ')
-console.log(a)
-const b=a.split('').reverse().join( '')
-console.log(b)
-}
-reverseString("openai is awesome")
+  // let even={}
